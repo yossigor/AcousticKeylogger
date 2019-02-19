6be51c846994adfffb66c303a6e8674fc7eab5b8
yossigor <yossigor@gmail.com>
2019-02-19 11:27:44 +0200

    modified version of S&T is now a submodule

--- Skype-Type_modified/dictionaries/README.md
+++
@@ -1,1 +0,0 @@
-Please put here your dictionary files, to leverage S&T against words and sentences.
--- Skype-Type_modified/dst/libraries/multiplier.py
+++
@@ -1,4 +0,0 @@
-def multiplier(in_queue, out_queues):
-    for res in iter(in_queue.get, None):
-        for out in out_queues:
-            out.put(res)
